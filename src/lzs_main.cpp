8d94dba4093951d2493f7310b0a5db5d8812b36d
lue <rnddim@gmail.com>
2015-02-12 16:32:10 -0800

    Add LzS decompresser.
    You can use this on files that are _just_ LzS, and not .gar.lzs

---
+++ src/lzs_main.cpp
@@ -0,0 +1,55 @@
+// lzs_main.cpp --- where the 'lzs' program starts
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+#include "lzs.hpp"
+#include "util.hpp"
+
+int main(int argc, char ** argv) {
+    if (argc != 3) {
+        std::cerr << "Usage:\n";
+        std::cerr << "    lzs <input file> <output file>\n";
+        return 1;
+    }
+
+    std::string infname  = argv[1];
+    std::string outfname = argv[2];
+
+    // open file
+
+    std::ifstream infile(infname, std::ios::binary | std::ios::ate);
+
+    if (infile.fail()) {
+        std::cerr << "Could not open \"" << infname << "\".\n";
+        return 1;
+    }
+
+    const int filesize = infile.tellg();
+    infile.seekg(0);
+
+    // see if it's an LZS file
+
+    char magic[4];
+    char filerawdata[filesize];
+    std::vector<uint8_t> filedata;
+
+    infile.read(magic, 4);
+
+    infile.seekg(0);
+    infile.read(filerawdata, filesize);
+    filedata = std::vector<uint8_t>(filerawdata, filerawdata + filesize);
+
+    if (magic == std::string("LzS\x01")) {
+        filedata = lzs_dec(filedata);
+        writeFile(outfname, "", (char*)filedata.data(), filedata.size());
+    } else {
+        std::cerr << "Not an LzS file.\n";
+        return 1;
+    }
+
+    return 0;
+}
